9cda366f64e30dc22de5e1a0b4ba756cb228fcb3
Hans-Peter Buniat <hpbuniat@gmail.com>
2013-01-20 00:14:18 +0100

    added new ticket-integrity constraint

---
+++ module/Flightzilla/src/Flightzilla/Model/Ticket/Integrity/Constraint/IllogicalSeverity.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * flightzilla
+ *
+ * Copyright (c) 2012-2013, Hans-Peter Buniat <hpbuniat@googlemail.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * * Neither the name of Hans-Peter Buniat nor the names of his
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package flightzilla
+ * @author Hans-Peter Buniat <hpbuniat@googlemail.com>
+ * @copyright 2012-2013 Hans-Peter Buniat <hpbuniat@googlemail.com>
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+namespace Flightzilla\Model\Ticket\Integrity\Constraint;
+
+use \Flightzilla\Model\Ticket\Type\Bug;
+use \Flightzilla\Model\Ticket\Source\Bugzilla;
+
+/**
+ * "Bugs" with a illogical severity
+ *
+ * @author Hans-Peter Buniat <hpbuniat@googlemail.com>
+ * @copyright 2012-2013 Hans-Peter Buniat <hpbuniat@googlemail.com>
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ * @version Release: @package_version@
+ * @link https://github.com/hpbuniat/flightzilla
+ */
+class IllogicalSeverity implements ConstraintInterface {
+
+    /**
+     * Name of the constraint
+     *
+     * @var string
+     */
+    const NAME = 'IllogicalSeverity';
+
+    /**
+     * (non-PHPdoc)
+     * @see ConstraintInterface::check()
+     */
+    public static function check(Bug $oTicket, Bugzilla $oTicketSource) {
+        $sSeverity = $oTicket->getSeverity();
+
+        return (($oTicket->isType($oTicket::TYPE_BUG) === true and ($sSeverity === Bug::SEVERITY_ENHANCEMENT or $sSeverity === Bug::SEVERITY_IMPROVEMENT)) === false);
+    }
+}
