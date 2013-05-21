4210e5aeebf2f3b0dd23d874e303d80d01a0ece7
Hans-Peter Buniat <hpbuniat@gmail.com>
2013-05-21 16:36:47 +0200

    better table sorting

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,10 +78,7 @@ module.exports = function (grunt) {
                         './components/jquery-ui/themes/smoothness/*.theme.css'
                     ], dest: './public/vendor/', filter: 'isFile', expand: true, flatten: true},
                     {src: ['./components/jquery-ui/jquery.ui.custom.min.js'], dest: './public/vendor/', filter: 'isFile', expand: true, flatten: true},
-                    {src: [
-                        './components/jquery.tablesorter/js/jquery.tablesorter.min.js',
-                        './components/jquery.tablesorter/js/jquery.metadata.js'
-                    ], dest: './public/vendor/', filter: 'isFile', expand: true, flatten: true}
+                    {src: ['./components/jquery.tablesorter/js/jquery.tablesorter.min.js',], dest: './public/vendor/', filter: 'isFile', expand: true, flatten: true}
                 ]
             }
         }
